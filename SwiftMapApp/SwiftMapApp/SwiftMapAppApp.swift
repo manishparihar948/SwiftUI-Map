3da42ce58049014a609537056722748a1c6588a9
manishparihar948 <manish.parihar948@gmail.com>
2023-09-01 07:12:17 +0200

    Project setup for Map in Swiftui

---
+++ SwiftMapApp/SwiftMapApp/SwiftMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftMapAppApp.swift
+//  SwiftMapApp
+//
+//  Created by Manish Parihar on 01.09.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
